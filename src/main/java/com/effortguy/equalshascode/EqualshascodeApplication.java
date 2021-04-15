2b6f4694f914c94a4e902562dd31518e57fde656
hyjung <hyjung@tmon.co.kr>
2021-04-15 15:49:23 +0900

    Initial commit

---
+++ src/main/java/com/effortguy/equalshascode/EqualshascodeApplication.java
@@ -0,0 +1,13 @@
+package com.effortguy.equalshascode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqualshascodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EqualshascodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/effortguy/equalshascode/EqualshascodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effortguy.equalshascode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqualshascodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
